0d6fb3400dedf4f3da888628ca31536b91f7529d
Jessica <jessicamwangi933@gmail.com>
2022-03-08 19:21:54 +0300

    added username, password and secret key exports

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ export MAIL_USERNAME='jessicamwangi933@gmail.com'
 export MAIL_PASSWORD='hello98'
 export SECRET_KEY='secret'
 # SQLALCHEMY_DATABASE_URI='DATABASE_URL'
-python3.6 manage.py server
+python3.10 manage.py server
